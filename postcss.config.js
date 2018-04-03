9759346fed5f8265433519bad8e8911f50dd34fe
prfi <przemyslaw.fieluba@mediaflex.pl>
2018-04-03 10:02:30 +0200

    css works

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        // require('lost'),
+        // require('postcss-smart-import')({/* ...options */}),
+        // require('precss')({/* ...options */}),
+        require('autoprefixer')({/* ...options */})
+    ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,61 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const entryDir = path.resolve(__dirname, 'assets/src');
+const buildDir = path.resolve(__dirname, 'assets/dist');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+const extractSass = new ExtractTextPlugin({
+  filename: '[name]',
+  disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+  entry: {
+    'index.css': [
+      entryDir + '/scss/index.scss',
+    ],
+  },
+  output: {
+    path: buildDir,
+    filename: '[name]',
+  },
+  watch: true,
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader', options: {
+                sourceMap: true,
+                url: false,
+              },
+            },
+            'postcss-loader',
+            {
+              loader: 'sass-loader', options: {
+                sourceMap: true,
+              },
+            }],
+          // use style-loader in development
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader?name=img/[name].[ext]',
+      },
+      {
+        test: /\.(eot|svg|ttf|woff)$/i,
+        loader: 'file-loader?name=fonts/[name].[ext]',
+      },
+    ],
+  },
+  plugins: [
+    extractSass,
+  ]
+};
