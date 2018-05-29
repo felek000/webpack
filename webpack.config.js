d93e2b75e9a3989a0c1d1acbce510f214c2ebaaf
prfi <przemyslaw.fieluba@mediaflex.pl>
2018-05-29 14:16:08 +0200

    fix source map

--- webpack.config.js
+++ webpack.config.js
@@ -7,68 +7,75 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 
 const extractSass = new ExtractTextPlugin({
-  filename: '[name]'
+    filename: '[name]'
 });
-module.exports = (env, options)=>{
-  console.log(`env: ${env} mode: ${options.mode}`);
-  return {
-    entry: {
-      'css/index.css': [
-        entryDir + '/scss/index.scss',
-      ],
-      'js/index.js': [
-        entryDir + '/js/index.js'
-      ],
-      'js/index2.js': [
-        entryDir + '/js/index2.js'
-      ],
-    },
-    output: {
-      path: buildDir,
-      publicPath: 'dist/',
-      filename: '[name]',
-    },
-    watch: ( options.mode == 'development' ) ? true : false,
-    devtool: ( options.mode == 'development' ) ? 'source-map' : false,
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          loader: extractSass.extract({
-            use: [
-              {
-                loader: 'css-loader', options: {
-                  url: false,
-                },
-              },
-              'postcss-loader',
-              {
-                loader: 'sass-loader', options: {},
-              }],
-            fallback: 'style-loader',
-          }),
+module.exports = (env, options) => {
+    console.log(`env: ${env} mode: ${options.mode}`);
+    return {
+
+        entry: {
+            'css/index.css': [
+                entryDir + '/scss/index.scss',
+            ],
+            'js/index.js': [
+                entryDir + '/js/index.js'
+            ],
+            'js/index2.js': [
+                entryDir + '/js/index2.js'
+            ],
         },
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-transform-runtime']
+
+        output: {
+            path: buildDir,
+            publicPath: 'dist/',
+            filename: '[name]',
+        },
+        devtool: ( options.mode == 'development' ) ? 'source-map' : false,
+        watch: ( options.mode == 'development' ) ? true : false,
+
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    loader: extractSass.extract({
+                        use: [
+                            {
+                                loader: 'css-loader', options: {
+                                    sourceMap: true,
+                                    url: false
+                                }
+                            },
+                            'postcss-loader',
+                            {
+                                loader: 'sass-loader',options: {
+                                    sourceMap: true,
+                                    url: false
+                                }
+                            }],
+                        fallback: 'style-loader',
+                    }),
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: ['@babel/plugin-transform-runtime']
+                        }
+                    }
+                }
+            ],
+        },
+        optimization: {
+            splitChunks: {
+                chunks: "all"
             }
-          }
-        }
-      ],
-    },
-    optimization: {
-      splitChunks: {
-          chunks: "all"
-      }
-    },
-    plugins: [
-      new CleanWebpackPlugin([buildDir]),
-      extractSass
-    ]
-  }
+        },
+        plugins: [
+            new CleanWebpackPlugin([buildDir]),
+            extractSass
+        ]
+    }
 };
